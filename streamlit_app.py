057060f34febddb177d0f3d8492edab588517af3
William Alexander Rodriguez Arroyo <35548531+williamxlr@users.noreply.github.com>
2024-07-08 22:41:17 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 WARADATA
+st.title("🎈 WARADATA")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
